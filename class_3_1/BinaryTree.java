dcd7daf88e4bf7c9b6cd8c69efe438c0ddb9e498
zhangyanan <455045952@qq.com>
2018-03-22 22:53:50 +0800

    树的表示

---
+++ class_3_1/BinaryTree.java
@@ -0,0 +1,12 @@
+package com.dsa.mooc.class_3_1;
+
+/**
+ * 二叉树的表示
+ */
+public class BinaryTree {
+    private class Node {
+        private int data;
+        private Node leftChild;
+        private Node rightChild;
+    }
+}
