88f63482bb9b417c842ccd95fd5563b0ed027b12
zhangyanan <455045952@qq.com>
2018-03-31 16:54:07 +0800

    二叉树同构

--- class_3_4/HomoTree.java
+++ class_3_4/HomoTree.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 /**
  * 树的同构：两棵树的根节点的两个子节点可以通过左右变换成同一颗树，这两个树就是同构的。
  * <p>
- * 1、树的表示：静态链表：数组+链表
- * 2、输的构建
+ * 1、树的表示：静态链表：数组+链表节点
+ * 2、树的构建
  * 3、同构判断
+ * <p>
+ * 重要的知识点：
+ * 1、如何构建一个树（静态链表）
+ * 2、如何寻找根节点（使用一个坐标数组flag来标记出现过的子树）
+ * 3、同构判别很考验逻辑。
  */
 
 public class HomoTree {
     private static class Node {
         private Object data;
-        private int leftChild;
+        private int leftChild;//注意这里的子节点是int类型，因为要保存子节点在数组中的位置
         private int rightChild;
 
         public Node(Object data) {
@@ -32,15 +37,17 @@ public class HomoTree {
         int N = sc.nextInt();//节点的个数
         Node[] t = new Node[N];
 
-        int[] flag = new int[N];//用来判断某个几点是不是子节点，是子节点的设为1
+        int[] flag = new int[N];//用来判断某个节点是不是子节点，是子节点的设为1
 
         for (int i = 0; i < N; i++) {//读取每一行信息
+
             //先把节点的信息（节点符号，左子节点，右子节点）读取出来
             Node node = new Node(sc.next());
-            tempLeft = sc.next();
+            tempLeft = sc.next();//读入的是String类型
             tempRight = sc.next();
+
             //判断左子节点和右子节点的情况，如果是-，设为-1，如果有子节点，则对应为相应子节点的坐标
-            //并且
+            //并且把出现过的子节点坐标，对应的flag数组的坐标标为1.
             if (tempLeft.equals('-')) {
                 node.leftChild = -1;
             } else {
@@ -64,25 +71,48 @@ public class HomoTree {
                 break;
             }
         }
+        //把树t和这棵树的头结点坐标root放入结果数组
         result[0] = t;
         result[1] = root;
 
-        return t;
+        return result;
     }
 
     //写逻辑
-    public static boolean isomorphism(Node[] n1, int root1, Node[] n2, int root2) {
-        if (root1 == -1 && root2 == -1) {//两个树都为空
+
+    /**
+     * 1、两个树都为空，同构；
+     * 2、一个空，一个不空，不同狗；
+     * 3、两个数的根节点不一样，不同狗；
+     * 4、两个左子树都空，那就判断两个右子树是否同构；两个左子树不同时空（两个树的左子树都不空，或者只有一个树的左子树空）：
+     * （1）如果树1.左和树2.左都空，只要比较树1.右和树2.右；
+     * （2）如果树1.左和树2.左都不空，并且值相等，那么比较树1.左和树2.左 && 树1.右和树2.右；
+     * （3）否则比较 树1.左和树2.右 && 树1.右和树2.左；
+     */
+    public static boolean isomorphism(Node[] n1, int r1, Node[] n2, int r2) {
+
+        if (r1 == -1 && r2 == -1) {//两个树都为空
             return true;
         }
-        if (root1 == -1 && root1 != -1 || root2 != -1 && root2 == -1) {//两个树中有一个树为空，另一个不空
+        if (r1 == -1 && r1 != -1 || r2 != -1 && r2 == -1) {//两个树中有一个树为空，另一个不空
             return false;
         }
-        if (!n1[root1].equals(n2[root2])) {//两个树的头结点不一样
+        if (!n1[r1].equals(n2[r2])) {//两个树的头结点不一样
             return false;
         }
-        //先比较一侧的树
-        if (n1)
+        //左子树为空,其子树根节点值相等，比较右子树。
+        if (n1[r1].leftChild == -1 && n2[r2].leftChild == -1) {
+            return isomorphism(n1, n1[r1].rightChild, n2, n2[r2].rightChild);
+        }
+        //左右子树都不为空，且值相等，那么比较树1.左和树2.左 && 树1.右和树2.右；
+        if (n1[r1].leftChild != -1 && n2[r2].leftChild != -1 &&
+                (n1[n1[r1].leftChild] == n2[n2[r2].leftChild])) {
+            return (isomorphism(n1, n1[r1].leftChild, n2, n2[r2].leftChild) &&
+                    isomorphism(n1, n1[r1].rightChild, n2, n2[r2].rightChild);
+        } else {//比较树1.左和树2.右 && 树1.右和树2.左；左右互换
+            return (isomorphism(n1, n1[r1].leftChild, n2, n2[r2].rightChild) &&
+                    isomorphism(n1, n1[r1].rightChild, n2, n2[r2].leftChild);
+        }
 
 
     }
